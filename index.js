75bbdbcc09f0c89bf9a37476c15eb22414b9c980
unknown <truongdtct1230@gmail.com>
2020-09-11 20:17:15 +0700

    install express and introduction

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', function(req,res){
+	res.send('<h1>hello my world !</h1>');
+})
+
+app.get('/user', function(red,res){
+	res.send('User list');
+})
+
+app.listen(port , function(){
+	console.log('server listening on port ' + port);
+})
