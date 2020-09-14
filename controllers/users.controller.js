1f4c1f88b858b0d28c289b945928847146877e7f
unknown <truongdtct1230@gmail.com>
2020-09-14 11:31:23 +0700

    example-nine controller

---
+++ controllers/users.controller.js
@@ -0,0 +1,39 @@
+
+const db = require('../db.js');
+const shortid = require('shortid');
+
+module.exports.index = function(red,res){
+	res.render('users/index',{
+		users: db.get('users').value()
+	});
+};
+
+module.exports.search = function(req,res){
+	var q = req.query.q;
+	var matchedUsers = db.get('users').value().filter(function(user){
+		return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
+	});
+	res.render('users/index', {
+		users: matchedUsers,
+		q: q
+	})
+};
+
+module.exports.create = function(req,res){
+	res.render('users/create');
+};
+
+module.exports.get = function(req,res){
+	var id = req.params.id; // lấy về params :id
+	var users = db.get('users').find({ id: id}).value();
+
+	res.render('users/view',{
+		user: users
+	});
+};
+
+module.exports.postCreate = function(req,res){
+	req.body.id = shortid.generate(); // lưu vào body một thuộc tính id 
+	db.get('users').push(req.body).write()
+	res.redirect('/user');
+};
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,44 +1,19 @@
-const express = require('express');
-const shortid = require('shortid');
 
-const db = require('../db.js');
+const express = require('express');
+const controller = require('../controllers/users.controller.js');
+
+
 const router = express.Router();
 
-router.get('/', function(red,res){
-	res.render('users/index',{
-		users: db.get('users').value()
-	});
-})
+router.get('/', controller.index);
 
-router.get('/search',function(req,res){
-	var q = req.query.q;
-	var matchedUsers = db.get('users').value().filter(function(user){
-		return user.name.toLowerCase().indexOf(q.toLowerCase()) != -1;
-	});
-	res.render('users/index', {
-		users: matchedUsers,
-		q: q
-	})
-})
+router.get('/search', controller.search);
 
-router.get('/create',function(req,res){
-	res.render('users/create');
-})
+router.get('/create', controller.create);
 
-router.get('/:id',function(req,res){
-	var id = req.params.id; // lấy về params :id
-	var users = db.get('users').find({ id: id}).value();
+router.get('/:id', controller.get);
 
-	res.render('users/view',{
-		user: users
-	});
-})
-
-router.post('/create',function(req,res){
-	req.body.id = shortid.generate(); // lưu vào body một thuộc tính id 
-	db.get('users').push(req.body).write()
-	res.redirect('/user');
-})
+router.post('/create', controller.postCreate);	
 
 
 module.exports = router;
